731f655b753bf2b690152b62f9da953360c152d8
Carl Green <carlgreen@gmail.com>
2011-10-17 23:26:10 +1300

    add print test for Slide Comp preset

--- mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/ProgramPrinterTest.java
+++ mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/ProgramPrinterTest.java
@@ -249,6 +249,21 @@ public class ProgramPrinterTest {
      * TODO would be good not to use SysexParser.parseProgram()?
      */
     @Test
+    public void testPrintSlideComp() throws Exception {
+        File expectedFile = new File(this.getClass().getClassLoader().getResource("015_Slide_Comp.txt").toURI());
+        String expected = readFile(expectedFile);
+        File preset = new File(this.getClass().getClassLoader().getResource("015_Slide_Comp.syx").toURI());
+        Program program = SysexParser.parsePrograms(preset).get(0);
+        String actual = ProgramPrinter.print(program);
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test printing a textual representation of the program.
+     *
+     * TODO would be good not to use SysexParser.parseProgram()?
+     */
+    @Test
     public void testPrintCWah() throws Exception {
         File expectedFile = new File(this.getClass().getClassLoader().getResource("064_C-Wah.txt").toURI());
         String expected = readFile(expectedFile);
