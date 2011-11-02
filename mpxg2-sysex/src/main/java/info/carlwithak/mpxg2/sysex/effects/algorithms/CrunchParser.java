d7a8f3d1d76f4645a7265bf88a24c51d691500c6
Carl Green <carlgreen@gmail.com>
2011-11-02 23:58:10 +1300

    add Crunch parser

---
+++ mpxg2-sysex/src/main/java/info/carlwithak/mpxg2/sysex/effects/algorithms/CrunchParser.java
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.Crunch;
+
+/**
+ * Class to parse parameter data for Crunch effect.
+ *
+ * @author Carl Green
+ */
+public class CrunchParser {
+
+    public static Crunch parse(byte[] effectParameters) {
+        Crunch crunch = new Crunch();
+
+        int lo = effectParameters[0] + effectParameters[1] * 16;
+        crunch.setLo(lo);
+
+        int mid = (byte) (effectParameters[2] + effectParameters[3] * 16);
+        crunch.setMid(mid);
+
+        int hi = (byte) (effectParameters[4] + effectParameters[5] * 16);
+        crunch.setHi(hi);
+
+        int inLevel = (byte) (effectParameters[6] + effectParameters[7] * 16);
+        crunch.setInLevel(inLevel);
+
+        int level = (byte) (effectParameters[8] + effectParameters[9] * 16);
+        crunch.setLevel(level);
+
+        return crunch;
+    }
+}
---
+++ mpxg2-sysex/src/test/java/info/carlwithak/mpxg2/sysex/effects/algorithms/CrunchParserTest.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.Crunch;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Carl Green
+ */
+public class CrunchParserTest {
+
+    @Test
+    public void testParse_TremAutoWah() throws ParseException {
+        byte[] effectParameters = {6, 0, 12, 0, 15, 0, 0, 0, 2, 3, 0, 2, 8, 2, 5, 1, 9, 2, 0, 0, 13, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Crunch crunch = CrunchParser.parse(effectParameters);
+        assertEquals(6, crunch.getLo());
+        assertEquals(12, crunch.getMid());
+        assertEquals(15, crunch.getHi());
+        assertEquals(0, crunch.getInLevel());
+        assertEquals(50, crunch.getLevel());
+    }
+}
