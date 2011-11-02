1fc4b8b4027d402c5fe301bdf1f3efb7290a0400
Carl Green <carlgreen@gmail.com>
2011-11-02 21:33:24 +1300

    add Aerosol parser

---
+++ mpxg2-sysex/src/main/java/info/carlwithak/mpxg2/sysex/effects/algorithms/AerosolParser.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.Aerosol;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import info.carlwithak.mpxg2.sysex.RateParser;
+import java.util.Arrays;
+
+/**
+ * Class to parse parameter data for Aerosol effect.
+ *
+ * @author Carl Green
+ */
+public class AerosolParser {
+
+    public static Aerosol parse(byte[] effectParameters) throws ParseException {
+        Aerosol aerosol = new Aerosol();
+
+        int mix = effectParameters[0] + effectParameters[1] * 16;
+        aerosol.setMix(mix);
+
+        int level = (byte) (effectParameters[2] + effectParameters[3] * 16);
+        aerosol.setLevel(level);
+
+        aerosol.setRate1(RateParser.parse("Rate1", Arrays.copyOfRange(effectParameters, 4, 10)));
+
+        int pulseWidth1 = (byte) (effectParameters[10] + effectParameters[11] * 16);
+        aerosol.setPulseWidth1(pulseWidth1);
+
+        int depth1 = (byte) (effectParameters[12] + effectParameters[13] * 16);
+        aerosol.setDepth1(depth1);
+
+        aerosol.setRate2(RateParser.parse("Rate2", Arrays.copyOfRange(effectParameters, 14, 20)));
+
+        int pulseWidth2 = (byte) (effectParameters[20] + effectParameters[21] * 16);
+        aerosol.setPulseWidth2(pulseWidth2);
+
+        int depth2 = (byte) (effectParameters[22] + effectParameters[23] * 16);
+        aerosol.setDepth2(depth2);
+
+        int resonance1 = (byte) (effectParameters[24] + effectParameters[25] * 16);
+        aerosol.setResonance1(resonance1);
+
+        int resonance2 = (byte) (effectParameters[26] + effectParameters[27] * 16);
+        aerosol.setResonance2(resonance2);
+
+        return aerosol;
+    }
+}
---
+++ mpxg2-sysex/src/test/java/info/carlwithak/mpxg2/sysex/effects/algorithms/AerosolParserTest.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.FrequencyRate;
+import info.carlwithak.mpxg2.model.effects.algorithms.Aerosol;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Carl Green
+ */
+public class AerosolParserTest {
+
+    @Test
+    public void testParse_ToeWahAero() throws ParseException {
+        byte[] effectParameters = {4, 6, 0, 0, 10, 1, 0, 0, 0, 0, 13, 2, 6, 4, 10, 1, 0, 0, 0, 0, 7, 3, 12, 3, 4, 12, 12, 3, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Aerosol aerosol = AerosolParser.parse(effectParameters);
+        assertEquals(100, aerosol.getMix());
+        assertEquals(0, aerosol.getLevel());
+        assertEquals(new FrequencyRate("Rate1", 0.26), aerosol.getRate1());
+        assertEquals(45, aerosol.getPulseWidth1());
+        assertEquals(70, aerosol.getDepth1());
+        assertEquals(new FrequencyRate("Rate2", 0.26), aerosol.getRate2());
+        assertEquals(55, aerosol.getPulseWidth2());
+        assertEquals(60, aerosol.getDepth2());
+        assertEquals(-60, aerosol.getResonance1());
+        assertEquals(60, aerosol.getResonance2());
+    }
+}
