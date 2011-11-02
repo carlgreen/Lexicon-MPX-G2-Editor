14a1cb0eddc28e30ed227f4a15f8eeb8619d3df0
Carl Green <carlgreen@gmail.com>
2011-11-03 00:15:04 +1300

    add Phaser parser

---
+++ mpxg2-sysex/src/main/java/info/carlwithak/mpxg2/sysex/effects/algorithms/PhaserParser.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.Phaser;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import info.carlwithak.mpxg2.sysex.RateParser;
+import java.util.Arrays;
+
+/**
+ * Class to parse parameter data for Phaser effect.
+ *
+ * @author Carl Green
+ */
+public class PhaserParser {
+
+    public static Phaser parse(byte[] effectParameters) throws ParseException {
+        Phaser phaser = new Phaser();
+
+        int mix = effectParameters[0] + effectParameters[1] * 16;
+        phaser.setMix(mix);
+
+        int level = (byte) (effectParameters[2] + effectParameters[3] * 16);
+        phaser.setLevel(level);
+
+        phaser.setRate(RateParser.parse("Rate", Arrays.copyOfRange(effectParameters, 4, 10)));
+
+        int pulseWidth = effectParameters[10] + effectParameters[11] * 16;
+        phaser.setPulseWidth(pulseWidth);
+
+        int depth = effectParameters[12] + effectParameters[13] * 16;
+        phaser.setDepth(depth);
+
+        int resonance = effectParameters[14] + effectParameters[15] * 16;
+        phaser.setResonance(resonance);
+
+        return phaser;
+    }
+
+}
---
+++ mpxg2-sysex/src/test/java/info/carlwithak/mpxg2/sysex/effects/algorithms/PhaserParserTest.java
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.FrequencyRate;
+import info.carlwithak.mpxg2.model.effects.algorithms.Phaser;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Carl Green
+ */
+public class PhaserParserTest {
+
+    @Test
+    public void testParse_Phaser() throws ParseException {
+        byte[] effectParameters = {4, 6, 0, 0, 3, 5, 0, 0, 0, 0, 2, 3, 4, 6, 14, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Phaser phaser = PhaserParser.parse(effectParameters);
+        assertEquals(100, phaser.getMix());
+        assertEquals(0, phaser.getLevel());
+        assertEquals(new FrequencyRate("Rate", 0.83), phaser.getRate());
+        assertEquals(50, phaser.getPulseWidth());
+        assertEquals(100, phaser.getDepth());
+        assertEquals(30, phaser.getResonance());
+    }
+}
