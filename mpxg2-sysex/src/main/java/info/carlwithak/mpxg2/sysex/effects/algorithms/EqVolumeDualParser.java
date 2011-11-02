d465634ac747afb18d40af424d38a2dac9668c71
Carl Green <carlgreen@gmail.com>
2011-11-02 23:53:39 +1300

    add EQ Volume (D) parser

---
+++ mpxg2-sysex/src/main/java/info/carlwithak/mpxg2/sysex/effects/algorithms/EqVolumeDualParser.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.EqVolumeDual;
+
+/**
+ * Class to parse parameter data for Volume (D) effect.
+ *
+ * @author Carl Green
+ */
+public class EqVolumeDualParser {
+
+    public static EqVolumeDual parse(byte[] effectParameters) {
+        EqVolumeDual volumeDual = new EqVolumeDual();
+
+        int mix = effectParameters[0] + effectParameters[1] * 16;
+        volumeDual.setMix(mix);
+
+        int level = (byte) (effectParameters[2] + effectParameters[3] * 16);
+        volumeDual.setLevel(level);
+
+        int volumeLeft = (byte) (effectParameters[4] + effectParameters[5] * 16);
+        volumeDual.setVolumeLeft(volumeLeft);
+
+        int volumeRight = (byte) (effectParameters[6] + effectParameters[7] * 16);
+        volumeDual.setVolumeRight(volumeRight);
+
+        return volumeDual;
+    }
+
+}
