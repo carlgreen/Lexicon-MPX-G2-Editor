e01d6ebeff9475c0ccd3fe6325903181da586533
Carl Green <carlgreen@gmail.com>
2011-11-03 19:38:39 +1300

    add Flanger24 (M) parser

---
+++ mpxg2-sysex/src/main/java/info/carlwithak/mpxg2/sysex/effects/algorithms/Flanger24MonoParser.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.effects.algorithms.Flanger24Mono;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import info.carlwithak.mpxg2.sysex.RateParser;
+import java.util.Arrays;
+
+/**
+ * Class to parse parameter data for Flanger24 (M) effect.
+ *
+ * @author Carl Green
+ */
+public class Flanger24MonoParser {
+
+    public static Flanger24Mono parse(byte[] effectParameters) throws ParseException {
+        Flanger24Mono flanger24Mono = new Flanger24Mono();
+
+        int mix = effectParameters[0] + effectParameters[1] * 16;
+        flanger24Mono.setMix(mix);
+
+        int level = (byte) (effectParameters[2] + effectParameters[3] * 16);
+        flanger24Mono.setLevel(level);
+
+        flanger24Mono.setRate(RateParser.parse("Rate", Arrays.copyOfRange(effectParameters, 4, 10)));
+
+        int pulseWidth = (byte) (effectParameters[10] + effectParameters[11] * 16);
+        flanger24Mono.setPulseWidth(pulseWidth);
+
+        int depth = (byte) (effectParameters[12] + effectParameters[13] * 16);
+        flanger24Mono.setDepth(depth);
+
+        int resonance = (byte) (effectParameters[14] + effectParameters[15] * 16);
+        flanger24Mono.setResonance(resonance);
+
+        int glide = (byte) (effectParameters[16] + effectParameters[17] * 16);
+        flanger24Mono.setGlide(glide);
+
+        int blend = (byte) (effectParameters[18] + effectParameters[19] * 16);
+        flanger24Mono.setBlend(blend);
+
+        return flanger24Mono;
+    }
+
+}
---
+++ mpxg2-sysex/src/test/java/info/carlwithak/mpxg2/sysex/effects/algorithms/Flanger24MonoParserTest.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright (C) 2011 Carl Green
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.carlwithak.mpxg2.sysex.effects.algorithms;
+
+import info.carlwithak.mpxg2.model.FrequencyRate;
+import info.carlwithak.mpxg2.model.effects.algorithms.Flanger24Mono;
+import info.carlwithak.mpxg2.sysex.ParseException;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author Carl Green
+ */
+public class Flanger24MonoParserTest {
+
+    @Test
+    public void testParse_Stomp() throws ParseException {
+        byte[] effectParameters = {4, 6, 0, 0, 15, 1, 0, 0, 0, 0, 2, 3, 4, 2, 2, 14, 2, 3, 2, 3, 7, 3, 12, 3, 4, 12, 12, 3, 4, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        Flanger24Mono flanger24Mono = Flanger24MonoParser.parse(effectParameters);
+        assertEquals(100, flanger24Mono.getMix());
+        assertEquals(0, flanger24Mono.getLevel());
+        assertEquals(new FrequencyRate("Rate", 0.31), flanger24Mono.getRate());
+        assertEquals(50, flanger24Mono.getPulseWidth());
+        assertEquals(36, flanger24Mono.getDepth());
+        assertEquals(-30, flanger24Mono.getResonance());
+        assertEquals(50, flanger24Mono.getGlide());
+        assertEquals(50, flanger24Mono.getBlend());
+    }
+}
