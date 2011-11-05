7000bcda0c4a4070ce34818138e0c42d5acb4142
Carl Green <carlgreen@gmail.com>
2011-11-05 18:32:52 +1300

    add EQ Volume (S) printer

--- mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/AlgorithmPrinterTest.java
+++ mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/AlgorithmPrinterTest.java
@@ -43,6 +43,7 @@ import info.carlwithak.mpxg2.model.effects.algorithms.EchoDual;
 import info.carlwithak.mpxg2.model.effects.algorithms.EchoMono;
 import info.carlwithak.mpxg2.model.effects.algorithms.EchoStereo;
 import info.carlwithak.mpxg2.model.effects.algorithms.EqVolumeDual;
+import info.carlwithak.mpxg2.model.effects.algorithms.EqVolumeStereo;
 import info.carlwithak.mpxg2.model.effects.algorithms.EqPedalVol;
 import info.carlwithak.mpxg2.model.effects.algorithms.FlangerMono;
 import info.carlwithak.mpxg2.model.effects.algorithms.FlangerStereo;
@@ -799,6 +800,19 @@ public class AlgorithmPrinterTest {
 
         String expected = "    Mix: 100%\n    Level: 0dB\n    Gain: +9\n    Fc: 393\n    Q: 0.1\n    Mode: LShlf\n";
         String actual = AlgorithmPrinter.print(oneBandMono);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPrintEqVolumeStereo() throws PrintException {
+        EqVolumeStereo volumeStereo = new EqVolumeStereo();
+        volumeStereo.setMix(100);
+        volumeStereo.setLevel(0);
+        volumeStereo.setVolume(0);
+
+        String expected = "    Mix: 100%\n    Level: 0dB\n    Vol: 0%\n";
+        String actual = AlgorithmPrinter.print(volumeStereo);
 
         assertEquals(expected, actual);
     }
