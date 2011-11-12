73e57503058e6be10a53c39fe01f50925269c901
Carl Green <carlgreen@gmail.com>
2011-11-12 16:52:13 +1300

    simplify guitar style test

--- mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/ProgramPrinterTest.java
+++ mpxg2-cli/src/test/java/info/carlwithak/mpxg2/printing/ProgramPrinterTest.java
@@ -44,24 +44,13 @@ public class ProgramPrinterTest {
         assertEquals("", ProgramPrinter.printGuitarStyles(program));
 
         program.setIsAcoustic(true);
-        assertEquals("Acoustic", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsBass(true);
-        assertEquals("Acoustic, Bass", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsBlues(true);
-        assertEquals("Acoustic, Bass, Blues", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsClean(true);
-        assertEquals("Acoustic, Bass, Blues, Clean", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsCountry(true);
-        assertEquals("Acoustic, Bass, Blues, Clean, Country", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsJazz(true);
-        assertEquals("Acoustic, Bass, Blues, Clean, Country, Jazz", ProgramPrinter.printGuitarStyles(program));
-
         program.setIsRock(true);
+
         assertEquals("Acoustic, Bass, Blues, Clean, Country, Jazz, Rock", ProgramPrinter.printGuitarStyles(program));
     }
 
