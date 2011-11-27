faae5ef2ccff6610830a7b6b7cef34d94bdb0b7b
Carl Green <carlgreen@gmail.com>
2011-11-27 22:47:31 +1300

    change EQ Volume (D) getters to return Parameter objects rather than primitives

--- mpxg2-cli/src/main/java/info/carlwithak/mpxg2/printing/effects/algorithms/EqVolumeDualPrinter.java
+++ mpxg2-cli/src/main/java/info/carlwithak/mpxg2/printing/effects/algorithms/EqVolumeDualPrinter.java
@@ -34,8 +34,8 @@ public class EqVolumeDualPrinter implements Printer {
         StringBuilder sb = new StringBuilder();
         sb.append("    Mix: ").append(ParameterPrinter.print(volumeDual.getMix())).append("\n");
         sb.append("    Level: ").append(ParameterPrinter.print(volumeDual.getLevel())).append("\n");
-        sb.append("    Vol-L: ").append(volumeDual.getVolumeLeft()).append("%\n");
-        sb.append("    Vol-R: ").append(volumeDual.getVolumeRight()).append("%\n");
+        sb.append("    Vol-L: ").append(ParameterPrinter.print(volumeDual.getVolumeLeft())).append("\n");
+        sb.append("    Vol-R: ").append(ParameterPrinter.print(volumeDual.getVolumeRight())).append("\n");
         return sb.toString();
     }
 
