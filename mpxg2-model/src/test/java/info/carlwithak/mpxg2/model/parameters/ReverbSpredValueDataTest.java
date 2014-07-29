467b1f05d8a9e4e7a4c589ba3a4f19217db3d19f
Carl Green <carlgreen@gmail.com>
2014-07-29 19:13:09 +1200

    fix up working test file for simplifying reverb spred

--- mpxg2-model/src/test/java/info/carlwithak/mpxg2/model/parameters/ReverbSpredValueDataTest.java
+++ mpxg2-model/src/test/java/info/carlwithak/mpxg2/model/parameters/ReverbSpredValueDataTest.java
@@ -16,6 +16,7 @@
  */
 package info.carlwithak.mpxg2.model.parameters;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import org.junit.Test;
 
@@ -28,16 +29,145 @@ import static org.junit.Assert.assertThat;
  */
 public class ReverbSpredValueDataTest {
 
+    private static final double[] TOP = {
+        25.6, 27.2, 28.8,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0, // 10
+        43.2, 44.8,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0, // 20
+        0,
+        0,
+        0,
+        0,
+        65.6,
+        0,
+        0,
+        0,
+        0,
+        0, // 30
+        0,
+        76.8,
+        0,
+        80.0,
+        0,
+        0,
+        84.8, 86.4,
+        0,
+        89.65, // 40
+        0,
+        0,
+        0,
+        96.0,
+        0,
+        0,
+        0,
+        102.6, 104.0,
+        0,
+        107.2,
+        0,
+        110.4, 112.0,
+        0,
+        0,
+        0,
+        118.4, 120.0,
+        0,
+        123.2, 124.8, 126.4,
+        0,
+        0,
+        0,
+        132.8,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        147.21,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        158.4,
+        0,
+        0,
+        0,
+        0,
+        0,
+        168.0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        179.25,
+        0,
+        182.4, 184.0,
+        0, // 100
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        236.8
+    };
+
     @Test
-    public void stuff() {
-        for (int i = 0; i < ReverbSpredValue.TOP.length; i++) {
-            int[] x = ReverbSpredValue.getRaw(i);
+    public void stuff() throws ReflectiveOperationException {
+        final Field f = ReverbSpredValue.class.getDeclaredField("REVERB_SPRED");
+        f.setAccessible(true);
+        final int[][] reverbSpred = (int[][]) f.get(null);
+        for (int i = 0; i < TOP.length; i++) {
+            int[] x = reverbSpred[i];
             if (x.length == 0) {
                 continue;
             }
             int[] y = new int[x.length];
             for (int j = 0; j < x.length; j++) {
-                y[j] = (int) (j * ReverbSpredValue.TOP[i] / x.length);
+                y[j] = (int) (j * TOP[i] / x.length);
             }
             System.out.println(i + " up to " + x[x.length - 1]);
             if (!Arrays.equals(x, y)) {
